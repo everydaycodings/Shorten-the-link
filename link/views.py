784abca1ee258a2b406c15a22aff752288c7c101
Kumar Saksham <everydaycodings@gmail.com>
2020-09-22 13:19:38 +0530

    added the api function

--- link/views.py
+++ link/views.py
@@ -8,10 +8,10 @@ def index(request):
         BITLY_ACCESS_TOKEN = "a091b1da90ec1b14fa0f616afbb7fa833e925bfd"
 
         b = bitly_api.Connection(access_token = BITLY_ACCESS_TOKEN) 
-            
+        global short_url
         linkget = request.POST.get("link")
         response = b.shorten(linkget) 
-        short_url = response["url"]
+        short_url = response["url"]       
         print(short_url)
         return redirect("result")
     
@@ -21,5 +21,7 @@ def index(request):
 
 
 def result(request):
+    url = short_url
+    context = {"link": url}
 
-    return render(request, "link/result.html")
+    return render(request, "link/result.html", context)
