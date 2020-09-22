4bb62c54321f18c51f18210a25c59e2f06372f8d
Kumar Saksham <everydaycodings@gmail.com>
2020-09-22 09:45:00 +0530

    add bitly API

--- link/views.py
+++ link/views.py
@@ -1,29 +1,25 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 import bitly_api
 # Create your views here.
 
 def index(request):
 
-    context = {}
-  
-    if request.method == "GET":
+    if request.method == "POST":
         BITLY_ACCESS_TOKEN = "a091b1da90ec1b14fa0f616afbb7fa833e925bfd"
 
         b = bitly_api.Connection(access_token = BITLY_ACCESS_TOKEN) 
-        
-        linkget = request.GET.get("link")
+            
+        linkget = request.POST.get("link")
         response = b.shorten(linkget) 
         short_url = response["url"]
         print(short_url)
-
-        context = {"url": short_url}
-    else:
-        print("not post")
+        return redirect("result")
     
 
-    return render(request, "link/index.html", context)
+    return render(request, "link/index.html")
 
 
 
 def result(request):
+
     return render(request, "link/result.html")
