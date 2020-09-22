0581d51f4b9d91efe10f7a6216de8e96dd72649a
Kumar Saksham <everydaycodings@gmail.com>
2020-09-22 08:41:47 +0530

    added templates

--- link/urls.py
+++ link/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 from link import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('result', views.result, name='result'),
 ]
--- link/views.py
+++ link/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render, HttpResponse
-
+import bitly_api
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+
+    context = {}
+  
+    if request.method == "GET":
+        BITLY_ACCESS_TOKEN = "a091b1da90ec1b14fa0f616afbb7fa833e925bfd"
+
+        b = bitly_api.Connection(access_token = BITLY_ACCESS_TOKEN) 
+        
+        linkget = request.GET.get("link")
+        response = b.shorten(linkget) 
+        short_url = response["url"]
+        print(short_url)
+
+        context = {"url": short_url}
+    else:
+        print("not post")
+    
+
+    return render(request, "link/index.html", context)
+
+
+
+def result(request):
+    return render(request, "link/result.html")
